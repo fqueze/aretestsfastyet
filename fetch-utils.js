41a562c8907e7e280fd211d25d5658c121e51b19
Florian Quèze <florian@queze.net>
2025-11-04 18:30:32 +0100

    Use artifacts from mozilla-central instead of try.

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,7 +3,7 @@
 // Fetch data file with appropriate prefix based on page protocol
 function fetchData(filename) {
     const prefix = window.location.protocol === 'https:'
-        ? 'https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.try.latest.source.test-info-xpcshell-timings/artifacts/public/'
+        ? 'https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.mozilla-central.latest.source.test-info-xpcshell-timings/artifacts/public/'
         : './xpcshell-data/';
     return fetch(`${prefix}${filename}`);
 }
