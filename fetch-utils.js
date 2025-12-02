d059c17e531a27b172fb870f840f952d023c91cc
Florian Quèze <florian@queze.net>
2025-12-02 17:18:30 +0100

    Use data from try for the fqueze.github.io fork.

--- fetch-utils.js
+++ fetch-utils.js
@@ -2,8 +2,13 @@
 
 // Fetch data file with appropriate prefix based on page protocol
 function fetchData(filename) {
-    const prefix = window.location.protocol === 'https:'
-        ? 'https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.mozilla-central.latest.source.test-info-xpcshell-timings/artifacts/public/'
-        : './xpcshell-data/';
+    let prefix;
+    if (window.location.protocol === 'https:') {
+        // Use try repository for fqueze.github.io, mozilla-central for others
+        const repository = window.location.hostname === 'fqueze.github.io' ? 'try' : 'mozilla-central';
+        prefix = `https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.${repository}.latest.source.test-info-xpcshell-timings/artifacts/public/`;
+    } else {
+        prefix = './xpcshell-data/';
+    }
     return fetch(`${prefix}${filename}`);
 }
